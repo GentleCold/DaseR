434a30855735c77f076fee08267e2d44e207b2bb
GentleCold <gentlecold@qq.com>
2026-04-10 10:48:55 +0800

    initial commit

--- conductor/tracks/visualize_call_graph_20260121/index.md
+++
@@ -1,5 +0,0 @@
-# 任务轨道 visualize_call_graph_20260121 上下文
-
-- [技术规范](./spec.md)
-- [实施计划](./plan.md)
-- [元数据](./metadata.json)
--- tests/test_codereview.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def test_codereview_dir_exists():
-    assert os.path.exists("codereview"), "codereview directory does not exist"
-    assert os.path.isdir("codereview"), "codereview is not a directory"
